cf47a1a8b3d73d4ba6fd06add343e836e7d9d76a
gdaniele-art <gdaniele@dc.uba.ar>
2026-05-10 13:15:54 -0300

    feat(mapper): enhance AgentMapper to include service response mapping and improve services count handling

--- backend/src/main/java/com/gdaniele_art/pinggo/mapper/AgentMapper.java
+++ backend/src/main/java/com/gdaniele_art/pinggo/mapper/AgentMapper.java
@@ -1,13 +1,22 @@
 package com.gdaniele_art.pinggo.mapper;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.gdaniele_art.pinggo.dto.AgentResponse;
 import com.gdaniele_art.pinggo.dto.CreateAgentRequest;
+import com.gdaniele_art.pinggo.dto.MonitoredServiceResponse;
 import com.gdaniele_art.pinggo.entity.Agent;
+import java.util.List;
 
 @Component
 public class AgentMapper {
+
+    @Autowired
+    private MonitoredServiceMapper monitoredServiceMapper;
+
     public Agent toEntity(CreateAgentRequest agentRequest){
         if (agentRequest == null) return null;
 
@@ -20,12 +29,39 @@ public class AgentMapper {
     public AgentResponse toResponse(Agent agent){
         if (agent == null) return null;
 
+        int servicesCount = agent.getMonitoredServices() == null ? 0: agent.getMonitoredServices().size();
+
         return AgentResponse.builder()
                 .id(agent.getId())
                 .name(agent.getName())
                 .description(agent.getDescription())
                 .enabled(agent.isEnabled())
-                .servicesCount(agent.getMonitoredServices().size())
+                .servicesCount(servicesCount)
                 .build();
+    }
+
+    public  AgentResponse toResponseWithService(Agent agent){
+        if(agent == null) return null;
+
+        List<MonitoredServiceResponse> services = new ArrayList();
+
+        if(agent.getMonitoredServices() != null){
+            services = agent.getMonitoredServices()
+                        .stream()
+                        .map(monitoredServiceMapper::toResponse)
+                        .toList();
+        }
+
+        AgentResponse response = AgentResponse.builder()
+                    .id(agent.getId())
+                    .name(agent.getName())
+                    .description(agent.getDescription())
+                    .enabled(agent.isEnabled())
+                    .servicesCount(services.size())
+                    .build();
+
+        response.setMonitoredServiceResponse(services);
+
+        return response;
     }
 }
