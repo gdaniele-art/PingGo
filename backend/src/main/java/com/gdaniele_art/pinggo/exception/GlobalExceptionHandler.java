901ea39b0fbaa7ca5b60af5885c6f5e7ff4968f3
gdaniele-art <gdaniele@dc.uba.ar>
2026-05-24 02:19:03 -0300

    refactor(services): replace generic exceptions with specific custom exceptions

--- backend/src/main/java/com/gdaniele_art/pinggo/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/gdaniele_art/pinggo/exception/GlobalExceptionHandler.java
@@ -3,14 +3,14 @@ package com.gdaniele_art.pinggo.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
+
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
+
 
 @RestController
 public class GlobalExceptionHandler {
